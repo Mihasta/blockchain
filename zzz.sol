89986ec720ec8087c3cefaaa26e201660d4aa31c
Mihasta <47403755+Mihasta@users.noreply.github.com>
2021-04-06 11:37:54 +0900

    Update zzz.sol

--- zzz.sol
+++ zzz.sol
@@ -1,45 +1,44 @@
-contract Owned
-{
-    address private owner;
+pragma solidity >=0.4.22;
+pragma experimental ABIEncoderV2;
+
+contract Owned{
+    address payable private owner;
     
-    constructor() public
-    {
+    constructor() public {
         owner = msg.sender;
     }
     
-    modifier OnlyOwner
-    {
-        require
-        (
+    modifier OnlyOwner{
+        require(
             msg.sender == owner,
             'Only owner can run this function!'
-        );
+            );
         _;
     }
     
-    function ChangeOwner(address newOwner) public OnlyOwner
-    {
+    function ChangeOwner(address payable newOwner) public OnlyOwner{
         owner = newOwner;
     }
     
-    function GetOwner() public returns (address)
-    {
+    function GetOwner() public returns (address){
         return owner;
     }
 }
 
-contract Test is Owned
-{
-    enum RequestType{NewHome, EditHome}
+contract ROSReestr is Owned{
+    enum RequestType {NewHome, EditHome}
+    
+    uint private price = 100 wei;
     
     struct Ownership
     {
         string homeAddress;
         address owner;
         uint p;
-    }   
+    }
     
-    struct Owner{
+    struct Owner
+    {
         string name;
         uint passSer;
         uint passNum;
@@ -53,37 +52,51 @@ contract Test is Owned
         uint area;
         uint cost;
     }
+    
     struct Request
     {
         RequestType requestType;
-        Home home;
+        string homeAddress;
+        uint area;
+        uint cost;
         uint result;
+        address adr;
+        bool isProcessed;
     }
+    
     struct Employee
     {
-        string nameEmployee;
+        string name;
         string position;
         string phoneNumber;
         bool isset;
     }
     
-    modifier OnlyEmployee
-    {
-        require
-        (
+    mapping(address => Employee) private employees;
+    mapping(address => Owner) private owners;
+    //key is request initiator
+    mapping(address => Request) private requests;
+    address[] requestInitiator;
+    mapping(string => Home) private homes;
+    mapping(string => Ownership[]) private ownerships;
+    
+    uint private amount;
+    
+    modifier OnlyEmployee {
+        require(
             employees[msg.sender].isset != false,
-            'Only Employee can run this function'
-        );
+            'Only Employee can run this function!'
+            );
         _;
     }
     
-    mapping(address => Employee) private employees;
-    mapping(address => Owner) private owners;
-    mapping(address => Request) private requests;
-    mapping(string => Home) private homes;
-    mapping(string => Ownership[]) private ownerships;
-    mapping(uint => address) private reqAdr;
-    uint id = 0;
+    modifier Costs(uint value){
+        require(
+            msg.value >= value,
+            'Not enough funds!!'
+            );
+        _;
+    }
     
     function AddHome(string memory _adr, uint _area, uint _cost) public {
         Home memory h;
@@ -92,47 +105,81 @@ contract Test is Owned
         h.cost = _cost;
         homes[_adr] = h;
     }
+    
     function GetHome(string memory adr) public returns (uint _area, uint _cost){
         return (homes[adr].area, homes[adr].cost);
     }
-    function AddEmployee(address _adr, string memory _name, string memory _pos, string memory _phone) public OnlyOwner {
+    
+    function AddEmployee(address empl, string memory _name, string memory _pos, string memory _phone) public OnlyOwner{
         Employee memory e;
-        e.nameEmployee = _name;
+        e.name = _name;
         e.position = _pos;
         e.phoneNumber = _phone;
         e.isset = true;
-        employees[_adr] = e;
+        employees[empl] = e;
     }
-    function GetEmployee(address adr) public OnlyOwner returns (string memory _name, string memory _pos, string memory _phone) {
-        return (employees[adr].nameEmployee, employees[adr].position, employees[adr].phoneNumber);
+    
+    function EditEmployee(address empl, string memory _name, string memory _pos, string memory _phone) public OnlyOwner{
+        employees[empl].name = _name;
+        employees[empl].position = _pos;
+        employees[empl].phoneNumber = _phone;
     }
-    function EditEmployee(address _adr, string memory _name, string memory _pos, string memory _phone) public OnlyOwner
+    
+    function GetEmployee(address empl) public OnlyOwner returns (string memory _name, string memory _pos, string memory _phone){
+        return (employees[empl].name, employees[empl].position, employees[empl].phoneNumber);
+    }
+    
+    function DeleteEmployee(address empl) public OnlyOwner returns (bool)
     {
-        employees[_adr].nameEmployee = _name;
-        employees[_adr].position = _pos;
-        employees[_adr].phoneNumber = _phone;
-    }
-     function DeleteEmployee(address _adr) public OnlyOwner
-    {
-        delete employees[_adr];
-    }
-    function AddRequestForNewHome(address _adr, string memory _homeAddress) public {
-        Request memory r;
-        r.requestType = RequestType.NewHome;
-        r.home = homes[_homeAddress];
-        requests[_adr] = r;
-        reqAdr[id] = _adr;
-        id = id + 1;
-    }
-    function GetRequests() public returns (string[] memory, uint[] memory, uint[] memory) {
-        string[] memory adr = new string[](id);
-        uint[] memory area = new uint[](id);
-        uint[] memory cost = new uint[](id);
-        for (uint i = 0; i < id; i++) {
-            adr[i] = requests[reqAdr[i]].home.homeAddress;
-            area[i] = requests[reqAdr[i]].home.area;
-            cost[i] = requests[reqAdr[i]].home.cost;
+        if (employees[empl].isset == true){
+            delete employees[empl];
+            return true;
         }
-        return (adr, area, cost);
+        return false;
     }
+    
+    function AddRequest(uint rType, string memory adr, uint area, uint cost, address newOwner) public Costs(price) payable returns (bool)
+    {
+        Request memory r;
+        r.requestType = rType == 0? RequestType.NewHome: RequestType.EditHome;
+        r.homeAddress = adr;
+        r.area = area;
+        r.cost = cost;
+        r.result = 0;
+        r.adr = rType==0?address(0):newOwner;
+        r.isProcessed = false;
+        requests[msg.sender] = r;
+        requestInitiator.push(msg.sender);
+        amount += msg.value;
+        return true;
+    }
+    
+    function GetRequest() public OnlyEmployee returns (uint[] memory, uint[] memory, string[] memory)
+    {
+        uint[] memory ids = new uint[](requestInitiator.length);
+        uint[] memory types = new uint[](requestInitiator.length);
+        string[] memory homeAddresses = new string[](requestInitiator.length);
+        for(uint i=0;i!=requestInitiator.length;i++){
+            ids[i] = i;
+            types[i] = requests[requestInitiator[i]].requestType == RequestType.NewHome ? 0: 1;
+            homeAddresses[i] = requests[requestInitiator[i]].homeAddress;
+        }
+        return (ids, types, homeAddresses);
+    }
+    
+    function ProcessedRequest(address _requestInitiator) public OnlyOwner returns (bool)
+    {
+        
+    }
+    
+    function EditCost(uint _price) public OnlyOwner
+    {
+        price = _price;
+    }
+    
+    function GetCost() public returns (uint)
+    {
+        return price;
+    }
+    
 }
